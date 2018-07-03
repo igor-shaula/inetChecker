9a4fd7c941c2fb47bdcf95db282f4018cdc59fdd
igor-shaula <igor.shaula@gmail.com>
2018-07-03 23:52:27 +0300

    setting up the project: versionName is now 0.0.1 \ FAB icon & main text are changed \\

--- main/src/main/java/com/igor_shaula/inetchecker/main/MainActivity.java
+++ main/src/main/java/com/igor_shaula/inetchecker/main/MainActivity.java
@@ -2,7 +2,6 @@ package com.igor_shaula.inetchecker.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -15,15 +14,17 @@ public class MainActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-      Toolbar toolbar = findViewById(R.id.toolbar);
+      
+      final Toolbar toolbar = findViewById(R.id.toolbar);
       setSupportActionBar(toolbar);
       
-      FloatingActionButton fab = findViewById(R.id.fab);
+      final FloatingActionButton fab = findViewById(R.id.fab);
       fab.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                     .setAction("Action", null).show();
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                     .setAction("Action", null).show();
+            fab.setImageResource(android.R.drawable.ic_media_pause);
          }
       });
    }
